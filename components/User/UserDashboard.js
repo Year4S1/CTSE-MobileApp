7427887d9ac7a295f6334c15f638bda7eb57eb03
Upendra-Ihalagedara <Upendra.Ihalagedara@studentambassadors.com>
2023-03-24 13:00:59 +0530

    updated user dashboard

--- components/User/UserDashboard.js
+++ components/User/UserDashboard.js
@@ -1,42 +1,105 @@
-import React from 'react'
+import React from "react";
 import {
-    HStack,
-    NativeBaseProvider,
-    Stack,
-    Button as NativeBaseButton,
-    VStack,
-    Center,
-  } from "native-base";
-  import {ImageBackground, StyleSheet, Text, View} from 'react-native';
+  HStack,
+  NativeBaseProvider,
+  Stack,
+  Button as NativeBaseButton,
+  VStack,
+  Center,
+} from "native-base";
+import { ImageBackground, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { firebase } from "../../Config";
 
 function UserDashboard() {
-    const image = {uri: 'https://images.unsplash.com/photo-1590487988256-9ed24133863e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGd5bXxlbnwwfHwwfHw%3D&w=1000&q=80'};
+  const image = {
+    uri: "https://images.unsplash.com/photo-1590487988256-9ed24133863e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGd5bXxlbnwwfHwwfHw%3D&w=1000&q=80",
+  };
+  const UserName = firebase.auth().currentUser.displayName;
   return (
     <View style={styles.container}>
-    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-      <Text style={styles.text}>Inside</Text>
-    </ImageBackground>
-  </View>
-    
-  )
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <Text style={styles.greeting}>Hello {UserName}</Text>
+        <View style={[{ width: "100%", height: 50, marginTop: 10 }]}>
+          <TouchableOpacity
+            style={{
+              height: 50,
+              backgroundColor: "#000000c0",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <Text style={{ fontSize: 22, color: "#FFFFFF" }}>Join a Gym</Text>
+          </TouchableOpacity>
+          
+        </View>
+        <View style={[{ width: "100%", height: 50, marginTop: 10 }]}>
+          <TouchableOpacity
+            style={{
+              height: 50,
+              backgroundColor: "#000000c0",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <Text style={{ fontSize: 22, color: "#FFFFFF" }}>Join a Gym</Text>
+          </TouchableOpacity>
+          
+        </View>
+        <View style={[{ width: "100%", height: 50, marginTop: 10 }]}>
+          <TouchableOpacity
+            style={{
+              height: 50,
+              backgroundColor: "#000000c0",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <Text style={{ fontSize: 22, color: "#FFFFFF" }}>Join a Gym</Text>
+          </TouchableOpacity>
+          
+        </View>
+        <View style={[{ width: "100%", height: 50, marginTop: 10 }]}>
+          <TouchableOpacity
+            style={{
+              height: 50,
+              backgroundColor: "#000000c0",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <Text style={{ fontSize: 22, color: "#FFFFFF" }}>Join a Gym</Text>
+          </TouchableOpacity>
+          
+        </View>
+      </ImageBackground>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    image: {
-      flex: 1,
-      justifyContent: 'center',
-    },
-    text: {
-      color: 'white',
-      fontSize: 42,
-      lineHeight: 84,
-      fontWeight: 'bold',
-      textAlign: 'center',
-      backgroundColor: '#000000c0',
-    },
-  });
+  container: {
+    flex: 1,
+  },
+  greeting: {
+    fontSize: 50,
+    fontWeight: "bold",
+    color: "#FFFFFF",
+    textAlign: "center",
+    marginTop: -150,
+    marginBottom: 200,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0",
+  },
+});
 
-export default UserDashboard
+export default UserDashboard;
