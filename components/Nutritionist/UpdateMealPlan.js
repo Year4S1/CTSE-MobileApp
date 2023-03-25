4bc811f4580ff4b89a313b01205eff39d57d8e74
danushka9999 <dranasinghe99@gmail.com>
2023-03-25 07:07:26 +0530

    feat/Nutritionist-Update meal plan initla added

---
+++ components/Nutritionist/UpdateMealPlan.js
@@ -0,0 +1,36 @@
+import {
+    Box,
+    Checkbox,
+    CheckIcon,
+    FormControl,
+    Input,
+    NativeBaseProvider,
+    Select,
+    Stack,
+    WarningOutlineIcon,
+    Button as NativeBaseButton,
+  } from "native-base";
+  import React, { useEffect, useState } from "react";
+  import {
+    Button,
+    Platform,
+    StyleSheet,
+    Text,
+    ToastAndroid,
+    View,
+  } from "react-native";
+  import { firebase } from "../../Config";
+  import DateTimePickerModal from "react-native-modal-datetime-picker";
+  import Footer from "../Common/Footer";
+  
+  
+  
+  const UpdateMealPlan = ({ route, navigation }) => {
+    
+  
+  
+
+  };
+  
+  export default UpdateMealPlan;
+  
