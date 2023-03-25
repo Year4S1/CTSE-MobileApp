396272f358b388b49c15f2742cc73f8be47b3178
danushka9999 <dranasinghe99@gmail.com>
2023-03-25 06:28:44 +0530

    feat/Nutritionist-View Nutritionist created meal plans inital

---
+++ components/Nutritionist/DisplayMealPlans.js
@@ -0,0 +1,16 @@
+import { NativeBaseProvider, ScrollView } from "native-base";
+import React, { cloneElement, useEffect, useState } from "react";
+import { Button, ImageBackground, Pressable, StyleSheet, View } from "react-native";
+import { Card, Paragraph, Title } from "react-native-paper";
+import { firebase } from "../../Config";
+import { AntDesign } from "@expo/vector-icons";
+import tw from 'twrnc';
+
+
+const DisplayMealPlans = ({ navigation }) => {
+  
+};
+
+export default DisplayWorkoutPlans;
+
+
