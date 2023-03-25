0187b4f6a7dfad3a20bee07dfa90f663b69a10eb
di-nethra <anjana.dinethra@gmail.com>
2023-03-25 10:42:32 +0530

    modification added

--- components/Trainer/AddWorkoutPlan.js
+++ components/Trainer/AddWorkoutPlan.js
@@ -20,6 +20,7 @@ import { useEffect } from "react";
 import Footer from "../Common/Footer";
 import image from "../../assets/lowOpWorkoutBack.png"
 import tw from 'twrnc';
+import { Linking } from "react-native";
 
 const AddWorkoutPlan = () => {
 
@@ -33,6 +34,8 @@ const AddWorkoutPlan = () => {
   const [userId, setUserId] = useState("");
   const [workoutSchedule, setWorkoutSchedule] = useState("");
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+
 
   const showDatePicker = () => {
     setDatePickerVisibility(true);
@@ -58,8 +61,9 @@ const AddWorkoutPlan = () => {
     setUserId(UserId);
   }, []);
 
+
   const addTestResults = () => {
-    
+
 
     firebase
       .firestore()
@@ -68,172 +72,176 @@ const AddWorkoutPlan = () => {
         UserName: userName,
         UserID: userId,
         WorkoutPlanId: workoutPlanId,
-        WorkoutType:workoutType,
+        WorkoutType: workoutType,
         BodyType: bodyType,
         SubDate: date,
         Intensity: intensity,
-        WorkoutSchedule:workoutSchedule,
+        WorkoutSchedule: workoutSchedule,
         visibility: visibility,
-        timeStamp:firebase.firestore.FieldValue.serverTimestamp()
+        timeStamp: firebase.firestore.FieldValue.serverTimestamp()
       })
       .then(() => {
         alert("Workout Plan Added!");
       });
+    sendMail()
   };
+
+
   return (
     <NativeBaseProvider >
-  {/* <ImageBackground source={image} resizeMode="cover"> */}
-        <View style={{ marginVertical: 30 }}>
-       
-          <Box w="100%">
-            <FormControl>
-              <Stack mx="8">
-                <Text style={tw`font-black mb-3`}>Workout Plan ID</Text>
-                <Input
-                  onChangeText={(val) => {
-                    setWorkoutId(val);
-                  }}
-                  type="text"
-                  placeholder="Please enter the Test ID"
-                />
-                <View style={{ marginVertical: 10 }}>
-                  <Text style={tw`font-black mb-3 `} >Workout Type</Text>
-                  <Select
-                    shadow={2}
-                    selectedValue={workoutType}
-                    minWidth="200"
-                    accessibilityLabel="Choose Workout Type"
-                    placeholder="please choose Workout Type"
-                    _selectedItem={{
-                      bg: "teal.600",
-                      endIcon: <CheckIcon size="5" />,
-                    }}
-                    onValueChange={(value) => setWorkoutType(value)}
-                  >
-                    <Select.Item
-                      shadow={2}
-                      label="Chest"
-                      value="chest"
-                    />
-                    <Select.Item
-                      shadow={2}
-                      label="Legs"
-                      value="legs"
-                    />
-                    <Select.Item
-                      shadow={2}
-                      label="Shoulders"
-                      value="shoulders"
-                    />
-                     <Select.Item
-                      shadow={2}
-                      label="Triceps"
-                      value="triceps"
-                    />
-                  </Select>
-                </View>
+      {/* <ImageBackground source={image} resizeMode="cover"> */}
+      <View style={{ marginVertical: 30 }}>
 
-                <View>
-                  <Text style={tw`font-black mb-3`}>Body Type</Text>
-                  <Select
-                    shadow={2}
-                    selectedValue={bodyType}
-                    minWidth="200"
-                    accessibilityLabel="choose Body Type"
-                    placeholder="please choose Body Type"
-                    _selectedItem={{
-                      bg: "teal.600",
-                      endIcon: <CheckIcon size="5" />,
-                    }}
-                    onValueChange={(value) => setBodyType(value)}
-                  >
-                    <Select.Item
-                      shadow={2}
-                      label="Skinny"
-                      value="skinny"
-                    />
-                    <Select.Item
-                      shadow={2}
-                      label="Average"
-                      value="Average"
-                    />
-                       <Select.Item
-                      shadow={2}
-                      label="Fat"
-                      value="fat"
-                    />
-                  </Select>
-                </View>
-
-                <View style={{ marginVertical: 10 }}>
-                  <Text style={tw`font-black mb-3`} >Date of Submitting</Text>
-                  <Input
-                    onPressIn={() => {
-                      showDatePicker();
-                    }}
-                    type="text"
-                    placeholder="Please enter the date"
-                    value={date}
-                  />
-                </View>
-                <View>
-                  <Text style={tw`font-black mb-3`} >Workout Intensity</Text>
-                </View>
+        <Box w="100%">
+          <FormControl>
+            <Stack mx="8">
+              <Text style={tw`font-black mb-3`}>Workout Plan ID</Text>
+              <Input
+                onChangeText={(val) => {
+                  setWorkoutId(val);
+                }}
+                type="text"
+                placeholder="Please enter the Test ID"
+              />
+              <View style={{ marginVertical: 10 }}>
+                <Text style={tw`font-black mb-3 `} >Workout Type</Text>
                 <Select
                   shadow={2}
-                  selectedValue={intensity}
+                  selectedValue={workoutType}
                   minWidth="200"
-                  accessibilityLabel="choose Intensity"
-                  placeholder="please choose Intensity"
+                  accessibilityLabel="Choose Workout Type"
+                  placeholder="please choose Workout Type"
                   _selectedItem={{
                     bg: "teal.600",
                     endIcon: <CheckIcon size="5" />,
                   }}
-                  onValueChange={(value) => setIntensity(value)}
+                  onValueChange={(value) => setWorkoutType(value)}
                 >
-                  <Select.Item shadow={2} label="High" value="high" />
-                  <Select.Item shadow={2} label="Medium" value="medium" />
-                  <Select.Item shadow={2} label="Low" value="low" />
-                </Select>
-                <View>
-                  <DateTimePickerModal
-                    isVisible={isDatePickerVisible}
-                    mode="date"
-                    onConfirm={handleConfirm}
-                    onCancel={hideDatePicker}
+                  <Select.Item
+                    shadow={2}
+                    label="Chest"
+                    value="chest"
                   />
-                </View>
-                <View style={{ marginVertical: 10 }}>
-                <Text style={tw`font-black mb-3`} >Workout plan Schedule</Text>
-                <TextArea  onChangeText={(val) => {
-                    setWorkoutSchedule(val);
-                  }} h={20} placeholder="Please enter the Workout plan Schedule" w="100%" maxW="500" />
-                </View>
+                  <Select.Item
+                    shadow={2}
+                    label="Legs"
+                    value="legs"
+                  />
+                  <Select.Item
+                    shadow={2}
+                    label="Shoulders"
+                    value="shoulders"
+                  />
+                  <Select.Item
+                    shadow={2}
+                    label="Triceps"
+                    value="triceps"
+                  />
+                </Select>
+              </View>
 
-                <View style={{ marginVertical: 10 }}>
-                  <Text style={tw`font-black mb-3`} >Workout plan visibility</Text>
-                  <Checkbox
-                    onChange={setVisibility}
-                    value={visibility}
-                    colorScheme="green"
-                  >
-                    Visibility
-                  </Checkbox>
-                </View>
-                <NativeBaseButton
-                  style={{ marginVertical: 15 }}
-                  onPress={addTestResults}
+              <View>
+                <Text style={tw`font-black mb-3`}>Body Type</Text>
+                <Select
+                  shadow={2}
+                  selectedValue={bodyType}
+                  minWidth="200"
+                  accessibilityLabel="choose Body Type"
+                  placeholder="please choose Body Type"
+                  _selectedItem={{
+                    bg: "teal.600",
+                    endIcon: <CheckIcon size="5" />,
+                  }}
+                  onValueChange={(value) => setBodyType(value)}
                 >
-                  Add Workout Plan
-                </NativeBaseButton>
-              </Stack>
-            </FormControl>
-          </Box>
+                  <Select.Item
+                    shadow={2}
+                    label="Skinny"
+                    value="skinny"
+                  />
+                  <Select.Item
+                    shadow={2}
+                    label="Average"
+                    value="Average"
+                  />
+                  <Select.Item
+                    shadow={2}
+                    label="Fat"
+                    value="fat"
+                  />
+                </Select>
+              </View>
 
-        </View>
-     
-        <View><Footer /></View>
-        {/* </ImageBackground> */}
+              <View style={{ marginVertical: 10 }}>
+                <Text style={tw`font-black mb-3`} >Date of Submitting</Text>
+                <Input
+                  onPressIn={() => {
+                    showDatePicker();
+                  }}
+                  type="text"
+                  placeholder="Please enter the date"
+                  value={date}
+                />
+              </View>
+              <View>
+                <Text style={tw`font-black mb-3`} >Workout Intensity</Text>
+              </View>
+              <Select
+                shadow={2}
+                selectedValue={intensity}
+                minWidth="200"
+                accessibilityLabel="choose Intensity"
+                placeholder="please choose Intensity"
+                _selectedItem={{
+                  bg: "teal.600",
+                  endIcon: <CheckIcon size="5" />,
+                }}
+                onValueChange={(value) => setIntensity(value)}
+              >
+                <Select.Item shadow={2} label="High" value="high" />
+                <Select.Item shadow={2} label="Medium" value="medium" />
+                <Select.Item shadow={2} label="Low" value="low" />
+              </Select>
+              <View>
+                <DateTimePickerModal
+                  isVisible={isDatePickerVisible}
+                  mode="date"
+                  onConfirm={handleConfirm}
+                  onCancel={hideDatePicker}
+                />
+              </View>
+              <View style={{ marginVertical: 10 }}>
+                <Text style={tw`font-black mb-3`} >Workout plan Schedule</Text>
+                <TextArea onChangeText={(val) => {
+                  setWorkoutSchedule(val);
+                }} h={20} placeholder="Please enter the Workout plan Schedule" w="100%" maxW="500" />
+              </View>
+
+              <View style={{ marginVertical: 10 }}>
+                <Text style={tw`font-black mb-3`} >Workout plan visibility</Text>
+                <Checkbox
+                  onChange={setVisibility}
+                  value={visibility}
+                  colorScheme="green"
+                >
+                  Visibility
+                </Checkbox>
+              </View>
+              <NativeBaseButton
+                style={{ marginVertical: 15 }}
+                onPress={addTestResults}
+              >
+                Add Workout Plan
+              </NativeBaseButton>
+           
+            </Stack>
+          </FormControl>
+        </Box>
+
+      </View>
+
+      <View><Footer /></View>
+      {/* </ImageBackground> */}
     </NativeBaseProvider>
   );
 };
--- components/Trainer/DisplayOtherWorkoutPlans.js
+++ components/Trainer/DisplayOtherWorkoutPlans.js
@@ -1,9 +1,11 @@
 
 import { NativeBaseProvider, ScrollView } from "native-base";
 import React, { useEffect, useState } from "react";
-import { Button, View } from "react-native";
+import { Button, ImageBackground, View } from "react-native";
 import { Card, Title } from "react-native-paper";
 import { firebase } from "../../Config";
+import image from "../../assets/displayPlansBack.jpg"
+import tw from 'twrnc';
 const DisplayOtherWorkoutPlans = () => {
   const [workoutPlans, setWorkoutPlans] = useState([]);
   const [currentRecordId, setCurrentRecordId] = useState([]);
@@ -28,6 +30,7 @@ const DisplayOtherWorkoutPlans = () => {
   }, []);
   return (
     <NativeBaseProvider>
+        <ImageBackground source={image} style={{flex:1}} resizeMode="cover">
       <ScrollView>
         {
           workoutPlans.map((item, index) => {
@@ -41,14 +44,14 @@ const DisplayOtherWorkoutPlans = () => {
                   borderRadius: 20,
                   boxShadow: '0 4px 30px rgba(70, 200, 250, 0.15)',
                 }}>
-                  <Card key={index}>
-                    <Card.Title title={`Workout Plan ${index + 1}`} />
+                  <Card key={index} style={tw`ml-3 max-w-sm rounded overflow-hidden shadow-lg rounded-3xl `}>
+                    <Card.Title style={tw`font-black text-xl mb-1`}  title={`Workout Plan ${index + 1}`} />
                     <Card.Content>
-                      <Title>Workout Plan ID :{item.WorkoutPlanId}</Title>
-                      <Title>Workout Plan Type :{item.WorkoutType}</Title>
-                      <Title>Body Type :{item.BodyType}</Title>
-                      <Title>Intensity :{item.Intensity}</Title>
-                      <Title>Added Date :{item.SubDate}</Title>
+                      <Title style={tw`inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2`}>Workout Plan ID :{item.WorkoutPlanId}</Title>
+                      <Title style={tw`inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2`}>Workout Plan Type :{item.WorkoutType}</Title>
+                      <Title style={tw`inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2`}>Body Type :{item.BodyType}</Title>
+                      <Title style={tw`inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2`}>Intensity :{item.Intensity}</Title>
+                      <Title style={tw`inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2`}>Added Date :{item.SubDate}</Title>
                     </Card.Content>
                   </Card>
                 </View>
@@ -58,6 +61,7 @@ const DisplayOtherWorkoutPlans = () => {
           })
         }
       </ScrollView>
+      </ImageBackground>
     </NativeBaseProvider>
   )
 };
